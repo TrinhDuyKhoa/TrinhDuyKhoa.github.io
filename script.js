cc98c4774f936de6f0d49eb76adf54288ff79aea
TrinhDuyKhoa <trinhkhoa247@gmail.com>
2024-10-20 13:37:58 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    const flowerContainer = document.querySelector('.flower-container');
+    const mainContainer = document.querySelector('.container');
+    const text_flower = document.querySelector('.text-flower');
+    const body = document.querySelector('body');
+
+    flowerContainer.style.display = 'block'; // Hiển thị hoa
+    mainContainer.style.display = 'none';
+    text_flower.style.display = 'block';
+    body.style.alignItems = "start"
+    const button = document.getElementById('surpriseButton');
+    button.textContent = "Bất ngờ đã đến!";
+    button.disabled = true;
+});
